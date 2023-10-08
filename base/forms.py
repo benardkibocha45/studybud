87f506275d9ed28f5b57d068314f7b35307f142f
benard kibocha <benardkibocha45@gmail.com>
2023-10-08 20:50:59 +0300

    Your commit message here

--- base/forms.py
+++ base/forms.py
@@ -1,13 +1,24 @@
-from .forms.user_forms import UserForm, MyUserCreationForm
-from .forms.room_forms import RoomForm
+from django.forms import ModelForm
+from .models import Room
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class RoomForm(ModelForm):
+    class  Meta:
+        model = Room
+        fields = '__all__'
+        exclude = ['host', 'participants']
+
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email'] 
 
 
 
+class MyUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['name', 'username', 'email', 'password1', 'password2']
 
-
-
-
-
-
-
-      
